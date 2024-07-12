1653bcb97d14f0810644cd4883161db30bb31daf
Damenhua <154488852+Damenhua@users.noreply.github.com>
2024-07-12 14:47:13 +0800

    finish

--- scripts.js
+++ scripts.js
@@ -3,47 +3,47 @@ const player = document.querySelector(".player");
 const viewer = player.querySelector(".viewer");
 const toggle = player.querySelector(".toggle");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector("progress__filled");
+const progressBar = player.querySelector(".progress__filled");
 const range = player.querySelectorAll(".player__slider");
 const skipBtn = player.querySelectorAll(".player__button");
 
 
 
 function togglePlay() {
-    const method = video.paused ? "play" : "pause";
-    video[method]();
+    const method = viewer.paused ? "play" : "pause";
+    viewer[method]();
 }
 
 function buttonUpdate() {
-    const btn = video.paused ? '►' : '❚ ❚';
+    const btn = viewer.paused ? '►' : '❚ ❚';
     toggle.textContent = btn;
 }
 
 function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
+    viewer.currentTime += parseFloat(this.dataset.skip);
 
 }
 
 function rangeHandler() {
-    video[this.name] = this.value;
+    viewer[this.name] = this.value;
 }
 
 function progressHandler() {
-    const percent = (video.currentTime/video.duration)*100;
+    const percent = (viewer.currentTime/viewer.duration)*100;
     progressBar.style.flexBasis = `${percent}%`;
 }
 
 function clickBar(e) {
-    const position = (e.offsetX/progress.offsetWidth)*video.duration;
-    video.currentTime = position
+    const position = (e.offsetX/progress.offsetWidth)*viewer.duration;
+    viewer.currentTime = position
 }
 
 
 
-video.addEventListener("click",togglePlay)
-video.addEventListener("play",buttonUpdate)
-video.addEventListener("pause",buttonUpdate)
-video.addEventListener("timeupdate",progressHandler)
+viewer.addEventListener("click",togglePlay)
+viewer.addEventListener("play",buttonUpdate)
+viewer.addEventListener("pause",buttonUpdate)
+viewer.addEventListener("timeupdate",progressHandler)
 
 toggle.addEventListener("click",togglePlay)
 skipBtn.forEach(b => b.addEventListener("click",skip))
@@ -51,12 +51,12 @@ range.forEach(r => r.addEventListener("change",rangeHandler))
 range.forEach(r => r.addEventListener("mousemove",rangeHandler))
 
 
-// progress.addEventListener("click",clickBar)
-// progress.addEventListener("mousemove",mousedown && clickBar)
+
+
 
 let mousedown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('click', clickBar);
+progress.addEventListener('mousemove', (e) => mousedown && clickBar(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 
